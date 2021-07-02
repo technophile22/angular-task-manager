616ae55b5c19901ba81066b8e7357d2e89c24809
technophile22 <raghavmangal22@gmail.com>
2021-07-02 19:20:29 +0530

    delete tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
 
 @Injectable({
@@ -13,5 +12,10 @@ export class TaskService {
 
 	getTasks(): Observable<Task[]> {
 		return this.http.get<Task[]>(this.apiUrl);
+	}
+
+	deleteTask(task: Task): Observable<Task> {
+		const url = `${this.apiUrl}/${task.id}`;
+		return this.http.delete<Task>(url);
 	}
 }
