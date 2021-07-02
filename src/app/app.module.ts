9482fee835c8363efb0b63850bd35258d71c1994
technophile22 <raghavmangal22@gmail.com>
2021-07-02 19:04:09 +0530

    http setup and connecting backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
@@ -9,8 +9,14 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent, TaskItemComponent],
-	imports: [BrowserModule, FontAwesomeModule],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		ButtonComponent,
+		TasksComponent,
+		TaskItemComponent,
+	],
+	imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 
@@ -6,9 +8,10 @@ import { Task } from '../Task';
 	providedIn: 'root',
 })
 export class TaskService {
-	constructor() {}
+	private apiUrl = 'http://localhost:5000/tasks';
+	constructor(private http: HttpClient) {}
 
-	getTasks(): Task[] {
-		return TASKS;
+	getTasks(): Observable<Task[]> {
+		return this.http.get<Task[]>(this.apiUrl);
 	}
 }
